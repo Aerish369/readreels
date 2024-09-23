0ac67a6715b75bd3309daba336332f028a957631
Aerish <aryalaerish369@gmail.com>
2024-09-23 17:32:35 +0545

    login logout added

--- readreels/urls.py
+++ readreels/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from bookreview import urls as bookreview_urls
+from users import urls as users_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(bookreview_urls)),
+    path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import logout
+from django.urls import reverse_lazy
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    success_url = '/home/'
+    redirect_authenticated_user = True
+
+    
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
