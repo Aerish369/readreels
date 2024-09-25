1d51fb2704208e6ae141f460feacc3f97e8a8ce8
Aerish <aryalaerish369@gmail.com>
2024-09-25 16:24:39 +0545

    user delete signal when profile deletes

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 from django.apps import apps
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.conf import settings 
-from django.dispatch import receiver
+from django.dispatch import receiver 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def createProfile(sender, instance, created, **kwargs):
@@ -11,3 +11,9 @@ def createProfile(sender, instance, created, **kwargs):
         profile = Profile.objects.create(
             user = instance,
         )
+
+
+@receiver(post_delete, sender=apps.get_model('bookreview', 'Profile'))
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
